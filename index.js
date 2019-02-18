2994b4c78e40e48df5a5889ca95018d0254462f7
Brent P. Newhall <brent@brentnewhall.com>
2019-02-18 11:00:08 -0500

    Added global removal of cats for all clients.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log( "Listening on port " + port );
 
 function init() {
     for( let index = 0; index < 5; index++ ) {
-        cats.push( { x: Math.random() * 500, y: Math.random() * 500 } );
+        cats.push( { x: parseInt(Math.random() * 500 + 50), y: parseInt(Math.random() * 500 + 50) } );
     }
 }
 
@@ -22,11 +22,14 @@ io.on( "connection", (socket) => {
     players.push( socket );
 	const player = players.length;
     console.log( "Player " + players.length + " connected." );
+	socket.emit( "player number", players.length );
 	for( let index = 0; index < cats.length; index++ ) {
 		socket.emit( "place cat", cats[index] );
 	}
-	socket.on( "collect", (cat) => {
-		console.log( "Player " + player + " collecting " + cat );
+	socket.on( "collect", (cat, index) => {
+		console.log( "Player " + player + " collecting " + cat + " " + index );
+		io.emit( "remove cat", cats[index] );
+		cats = cats.splice( index, 1 );
 	});
 	socket.on( 'disconnect', () => {
 		players = players.splice(player,1);
